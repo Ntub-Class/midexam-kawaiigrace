285b3de3ee37f8d2ee704e916caabe0ef9352077
kawaiigrace <55239599+kawaiigrace@users.noreply.github.com>
2019-11-07 20:23:58 +0800

    Add files via upload

---
+++ q2.ts
@@ -0,0 +1,9 @@
+// 題目說明: 請使用套件readline-sync跟node-qrcode，輸入文字來產生QRCODE
+// 輸入說明: 請打上任意文字，並產生QRCODE
+// 輸出說明: 請輸出檔名為QRCODE.png圖片
+import * as realineSync from 'readline-sync'; 
+import * as QRCode from 'qrcode';
+//create QRcode  \
+let show = realineSync.question('PleaseEnterWhateverYouWantToShow'); 
+QRCode.toFile('./QRCODE.png', show)
+console.log('Create QRcode successfully!')
